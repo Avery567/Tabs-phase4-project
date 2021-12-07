cd591f23042f7e69a60055a6cb2f21161c641a52
Avery Lin <tclin100@gmail.com>
2021-12-06 19:37:43 -0500

    revised login backend

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import { Button, Modal, Form, Input } from 'antd';
 import { useState } from 'react';
 
-function Login({onLogin}) {
+function Login() {
     const [isLoginVisible, setLoginVisible] = useState(false)
     const [loginInfo, setLoginInfo] = useState({
         username: "",
